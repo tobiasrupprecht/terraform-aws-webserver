f8633f2a30e7a2ec8b1386bbff0d42e86b167076
tobiasrupprecht <trupprecht@hashicorp.com>
2021-06-10 11:28:22 +0200

    change module

--- variables.tf
+++ variables.tf
@@ -1,4 +1,15 @@
-variable "env" {
+variable "instance_name" {
+  description = "Value of the Name tag for the EC2 instance"
   type        = string
-  default     = "local"
+  default     = "ExampWebserverInstance"
+}
+variable "region" {
+}
+variable "availability_zone" {
+}
+variable "instance_type" {
+}
+variable "ami" {
+}
+variable "env" {
 }
